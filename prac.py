b8f287af0ac0725421daa8ccc24eafbad3a1d9be
gayoung <lgy4625@naver.com>
2023-01-29 20:52:52 +0900

    ha

--- prac.py
+++ prac.py
@@ -1,12 +1,22 @@
 import sys
-
 input = sys.stdin.readline
 
-T = int(input())
+T = int(input()) #3
 
-for i in range(T):
-    line = list((input().rstrip().split()))
+for i in range(T): #0, 1, 2
+    l = list(map(int, input().split())) #4 5 6 쪼개 4,5,6 
     # A, B, C, D = map(int, input().rstrip().split())
-
-
-print(line)
+print(l)
+#------------------------------------------
+# T = int(input())
+# for test_case in range(1, T+1):
+#     num = list(map(int,input().split()))
+#     avg = sum(num)/len(num)
+#     avg = round(avg)
+#     print("#{} {}".format(test_case, avg))
+# #------------------------------------------
+# num = int(input())
+# #print(T)
+# for i in range(num):
+#     l = list(map(int,input().split()))
+#     l.sort()
