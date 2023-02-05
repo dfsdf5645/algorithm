3746f4ee8c4e7f58e8ed2a4df8a60e817476ad5c
gygy <122499571+dfsdf5645@users.noreply.github.com>
2023-02-05 11:18:32 +0900

    Study1 (#2)
    * 15904
    
    * two
    
    * ha
    
    * jonhim
    
    * si
    
    * ha

---
+++ 15904.py
@@ -0,0 +1,25 @@
+#UCPC 약자
+import sys
+input = sys.stdin.readline
+words = input()
+
+target = ['U', 'C', 'P', 'C']
+
+upper = ''
+for char in words:
+    if char.isupper() == True:
+        upper += char
+
+idx = 0
+result = ''
+for A in upper:
+  if result == 'UCPC':
+    break
+  elif A == target[idx]:
+    result += A
+    idx += 1
+
+if result == 'UCPC':
+    print('I love UCPC')
+else:
+    print('I hate UCPC')
